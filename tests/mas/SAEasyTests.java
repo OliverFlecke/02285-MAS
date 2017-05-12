9ddc119f8a2d0c7fbd054cf79d6dd573d0fdcc7a
Oliver Fleckenstein <oliverfl@live.dk>
2017-05-12 09:41:14 +0200

    Added more tests

--- tests/mas/SAEasyTests.java
+++ tests/mas/SAEasyTests.java
@@ -6,27 +6,12 @@ public class SAEasyTests {
 
 	private static final String PATH = "single_agent\\easy\\";
 	
-	@Test
-	public void SAanagram() { Tester.testLevel("SAanagram"); }
-	
-	@Test
-	public void SAbruteforce() { Tester.testLevel(PATH + "SAbruteforce"); }
-	
-	@Test
-	public void SACrunch() { Tester.testLevel(PATH + "SACrunch"); }
-	
-	@Test
-	public void SADeliRobot() { Tester.testLevel(PATH + "SADeliRobot"); }
-	
-	@Test
-	public void SAFirefly() { Tester.testLevel(PATH + "SAFirefly"); }
-	
-	@Test
-	public void SAgroup42() { Tester.testLevel(PATH + "SAgroup42"); }
-	
-	@Test
-	public void SARyther() { Tester.testLevel(PATH + "SARyther"); }
-	
-	@Test
-	public void SAsampdoria() { Tester.testLevel(PATH + "SAsampdoria"); }
+	@Test public void SAanagram() 		{ Tester.testLevel("SAanagram"); }
+	@Test public void SAbruteforce() 	{ Tester.testLevel(PATH + "SAbruteforce"); }
+	@Test public void SACrunch() 		{ Tester.testLevel(PATH + "SACrunch"); }
+	@Test public void SADeliRobot() 	{ Tester.testLevel(PATH + "SADeliRobot"); }
+	@Test public void SAFirefly() 		{ Tester.testLevel(PATH + "SAFirefly"); }
+	@Test public void SAgroup42() 		{ Tester.testLevel(PATH + "SAgroup42"); }
+	@Test public void SARyther() 		{ Tester.testLevel(PATH + "SARyther"); }
+	@Test public void SAsampdoria() 	{ Tester.testLevel(PATH + "SAsampdoria"); }
 }
---
+++ tests/mas/SAMediumTests.java
@@ -0,0 +1,19 @@
+package mas;
+
+import org.junit.Test;
+
+public class SAMediumTests {
+
+	private final static String PATH = "single_agent\\medium\\";
+	
+	@Test public void SAAlpha() 	{ Tester.testLevel(PATH + "SAAlpha"); }
+    @Test public void SAAteam()		{ Tester.testLevel(PATH + "SAAteam"); }
+    @Test public void SABullFight()	{ Tester.testLevel(PATH + "SABullFight"); }
+    @Test public void SADCN()		{ Tester.testLevel(PATH + "SADCN"); }
+    @Test public void SAFOAM()		{ Tester.testLevel(PATH + "SAFOAM"); } 
+    @Test public void SAholdkaeft()	{ Tester.testLevel(PATH + "SAholdkaeft"); }
+    @Test public void SAnull()		{ Tester.testLevel(PATH + "SAnull"); } 
+    @Test public void SATalos()		{ Tester.testLevel(PATH + "SATalos"); }
+    @Test public void SATeamNoOp()	{ Tester.testLevel(PATH + "SATeamNoOp"); }
+    @Test public void SAwallE()		{ Tester.testLevel(PATH + "SAwallE"); }
+}
