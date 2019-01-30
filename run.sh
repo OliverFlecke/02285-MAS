a885c22ad8988ac8d719b90fb82efd8c0971ced9
Oliver Fleckenstein <Oliverfl@live.dk>
2019-01-30 21:15:43 +0100

    Removed old build file and fixed run script for running on linux

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-build.sh
-java -jar server.jar -l $1 -c "java -Xmx4g -jar client.jar" -g 100 -t 300 $2
+echo "NOTE: You need to manually recompile the jar before running"
+java -Dsun.java2d.opengl=true -jar server.jar -l $1 -c "java -Xmx4g -jar client.jar" -g 500 -t 300 $2
